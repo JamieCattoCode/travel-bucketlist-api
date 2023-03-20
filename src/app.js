d2ce70ae865001c13ead9d0425567807e352901c
Jamie Catto <jamietcatto@gmail.com>
2023-03-20 08:39:40 +0100

    Added JWT authentication to login and register controllers with cookies

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const userRouter = require('./routes/user');
 const destinationRouter = require('./routes/destination');
 const listRouter = require('./routes/list');
 const favouriteRouter = require('./routes/favourite');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
 app.use(express.json());
 app.use(
     cors({
-        origin: '*',
-        methods: ['GET', 'POST', 'PATCH']
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PATCH'],
+        credentials: true
     })
 );
+app.use(cookieParser());
 
 app.use('/users', userRouter);
 app.use('/destinations', destinationRouter);
 app.use('/lists', listRouter);
 app.use('/favourites', favouriteRouter);
+app.use('/auth', authRouter);
 
 module.exports = app;
---
+++ src/controllers/auth.js
@@ -0,0 +1,88 @@
+const { User } = require('../models');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const TOKEN_KEY = 'Jg5pp8H4qVLr';
+
+exports.register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+    
+        if(!(username && email && password)) {
+            return res.status(400).json({message: 'You need to enter a username, email, and password.'});
+        }
+
+        const existingUser = await User.findOne({ where: 
+            {
+                username: username, 
+                email: email
+            } 
+        });
+
+        if (existingUser) res.status(409).json({message: `User ${username} already exists.`});
+
+        const encryptedPassword = bcrypt.hashSync(password, 10);
+        const newUser = await User.create({
+            username,
+            email: email.toLowerCase(),
+            password: encryptedPassword
+        });
+
+        const token = jwt.sign(
+            { username: newUser.username, email: newUser.email },
+            TOKEN_KEY
+            );
+
+        const newUserWithToken = {...newUser, token: token};
+
+        return res.status(201)
+        .cookie(
+            'userToken', 
+            token, 
+            {maxAge: 1000*60*60*2}) // 2 hours maxAge
+        .send({
+            status: 201,
+            message: 'New user created.'
+        })
+        //.json({user: newUserWithToken});
+        
+    } catch (err) {
+        res.status(400);
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+    
+        if (!(username && email && password)) {
+            return res.status(400).send('You need to enter a username, email, and password.');
+        }
+
+        const user = await User.findOne({ where: 
+            {
+                username: username, 
+                email: email
+            } 
+        });
+    
+        if (user && (await bcrypt.compare(password, user.password))) {
+            const token = jwt.sign(
+                { username: user.username, email: user.email },
+                TOKEN_KEY,
+            );
+
+            return res.status(201)
+            .cookie('userToken', token, {maxAge: 1000*60*60*2}) // 2 hours maxAge
+            .send({
+                status: 201,
+                message: 'User logged in.'
+            });
+            // .json({user: userWithToken});
+
+        } else return res.status(400).send('Invalid credentials entered.');
+
+        } catch (err) {
+            console.log(err);
+        }
+}
