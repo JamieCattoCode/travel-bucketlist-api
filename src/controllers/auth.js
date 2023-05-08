5bd3ddc8f3b1f44bd7d1e7ea973f09f209b8dd1a
JamieCattoCode <jamietcatto@gmail.com>
2023-05-08 16:12:41 +0200

    Removed register method from auth controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,50 +4,6 @@ const jwt = require('jsonwebtoken');
 
 const TOKEN_KEY = process.env.TOKEN_KEY;
 
-exports.register = async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
-    
-        if(!(username && email && password)) {
-            return res.status(400).json({message: 'You need to enter a username, email, and password.'});
-        }
-
-        const existingUser = await User.findOne({ where: 
-            {
-                username: username, 
-                email: email
-            } 
-        });
-
-        if (existingUser) res.status(409).json({message: `User ${username} already exists.`});
-
-        const encryptedPassword = bcrypt.hashSync(password, 10);
-        const newUser = await User.create({
-            username,
-            email: email.toLowerCase(),
-            password: encryptedPassword
-        });
-
-        const token = jwt.sign(
-            { user: newUser },
-            TOKEN_KEY
-            );
-
-        return res.status(201)
-        .cookie(
-            'userToken', 
-            token, 
-            {maxAge: 1000*60*60*2}) // 2 hours maxAge
-        .send({
-            status: 201,
-            message: 'New user created.'
-        })
-        
-    } catch (err) {
-        res.status(400).json({ error: err });
-    }
-}
-
 exports.login = async (req, res) => {
     try {
         const { username, password } = req.body;
